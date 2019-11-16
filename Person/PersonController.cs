8f2fed04e5c91e551e8355f22b549978f2af9939
Rozum Roman <iamkirakami@gmail.com>
2019-11-16 15:13:59 +0300

    Movement main pers

--- Person/PersonController.cs
+++ Person/PersonController.cs
@@ -1,8 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Person
 {
     public class PersonController : MonoBehaviour
     {
+        public float moveSpeed = 100f;
+        public Rigidbody2D rb;
+
+        private Vector3 _movement;
+
+        private void Update()
+        {
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = Input.GetAxisRaw("Vertical");
+            _movement.Normalize();
+            Vector3 vector = _movement * moveSpeed;
+            rb.AddForce(vector * Time.deltaTime);
+        }
     }
 }
